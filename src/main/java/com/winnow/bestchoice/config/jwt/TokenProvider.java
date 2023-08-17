0ab65f6e4ce18571b2bbd12f3faa51bf50c35951
kimsungho <tigertjdgh@gmail.com>
2023-08-18 00:26:55 +0900

    feat: JWT 토큰 생성 기능 추가 및 테스트 코드

---
+++ src/main/java/com/winnow/bestchoice/config/jwt/TokenProvider.java
@@ -0,0 +1,80 @@
+package com.winnow.bestchoice.config.jwt;
+
+import com.winnow.bestchoice.domain.Member;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Service
+@Getter
+public class TokenProvider {
+
+    private final JwtProperties jwtProperties;
+
+    public String generateToken(Member member, Duration expiredAt) {
+        Date now = new Date();
+        return makeToken(new Date(now.getTime() + expiredAt.toMillis()), member);
+    }
+
+    // JWT 토큰 생성 메서드
+    private String makeToken(Date expiry, Member member) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 typ : JWT
+                .setIssuer(jwtProperties.getIssuer()) // 발행하는 사람
+                .setIssuedAt(now) // 발행일자 -> 현재 시간
+                .setExpiration(expiry)
+                .setSubject(member.getEmail())
+                .claim("id", member.getId())
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+
+    // JWT 토큰 유효성 검증 메서드
+    public boolean validToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey()) // 복호화
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) { // 복호화 과정에서 에러가 나면 유효하지 않은 토큰
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UsernamePasswordAuthenticationToken(new org.springframework.security.core.userdetails.User(
+                claims.getSubject(), "", authorities), token, authorities);
+    }
+
+    // 토큰 기반으로 유저 ID를 가져오는 메서드
+    public Long getUserId(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("id", Long.class);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+}
