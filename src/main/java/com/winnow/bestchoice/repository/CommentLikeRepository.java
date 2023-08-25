7cb9883d2c7248813ad584e6919da0cbb5d971e3
yuun-dev <yuun.dev@gmail.com>
2023-08-25 10:41:36 +0900

    feat: 댓글 좋아요 취소 기능 구현

--- src/main/java/com/winnow/bestchoice/repository/CommentLikeRepository.java
+++ src/main/java/com/winnow/bestchoice/repository/CommentLikeRepository.java
@@ -5,7 +5,11 @@ import com.winnow.bestchoice.entity.CommentLike;
 import com.winnow.bestchoice.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 
     boolean existsByCommentAndMember(Comment comment, Member member);
+
+    Optional<CommentLike> findByCommentAndMember(Comment comment, Member member);
 }
