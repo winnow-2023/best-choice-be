2f11e640a94e359c6b72d5605f3f33783433ca1b
yuun-dev <yuun.dev@gmail.com>
2023-08-28 20:50:35 +0900

    refactor: 댓글 좋아요 count update 로직 수정

--- src/main/java/com/winnow/bestchoice/repository/CommentRepository.java
+++ src/main/java/com/winnow/bestchoice/repository/CommentRepository.java
@@ -5,9 +5,19 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @EntityGraph(attributePaths = "member") //member 같이 조회 - 조회 컬럼 최적화하기
     Page<Comment> findByPost_Id(long post_id, Pageable pageable);
+
+    @Modifying
+    @Query("update Comment c set c.likeCount=c.likeCount+1 where c.id=:id")
+    void plusLikeCountById(long id);
+
+    @Modifying
+    @Query("update Comment c set c.likeCount=c.likeCount-1 where c.id=:id")
+    void minusLikeCountById(long id);
 }
