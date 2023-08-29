6615775d5fda221f51e53c04e8797dd65f4adf46
winnow-2023 <142189987+winnow-2023@users.noreply.github.com>
2023-08-30 00:01:37 +0900

    Create TestController.java
    테스트용 컨트롤러 생성

---
+++ src/main/java/com/winnow/bestchoice/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.winnow.bestchoice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "환영합니다 베스트초이스 백엔드 서버입니다. ^_^";
+    }
+}
