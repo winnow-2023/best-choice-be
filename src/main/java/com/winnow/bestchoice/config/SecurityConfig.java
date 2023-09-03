eedcd91d28d13095cad6f86dfadbb60824356e8d
82102 <tigertjdgh@gmail.com>
2023-09-03 16:27:11 +0900

    chore : 스프링 시큐리티 설정 변경 및 인덱스 페이지 생성

--- src/main/java/com/winnow/bestchoice/config/SecurityConfig.java
+++ src/main/java/com/winnow/bestchoice/config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                     .cors()
+                    .configurationSource(corsConfigurationSource())
                 .and()
                     .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
