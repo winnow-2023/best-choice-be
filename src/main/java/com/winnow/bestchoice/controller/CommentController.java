2ee0b45c3f9fd221e7d30a3892e6b4b12d08b527
yuun <nohyunil3008@gmail.com>
2023-09-04 21:21:22 +0900

    update: url (+ /api)

--- src/main/java/com/winnow/bestchoice/controller/CommentController.java
+++ src/main/java/com/winnow/bestchoice/controller/CommentController.java
@@ -1,33 +1,50 @@
 package com.winnow.bestchoice.controller;
 
+import com.winnow.bestchoice.model.request.CreateCommentForm;
 import com.winnow.bestchoice.service.CommentService;
+import com.winnow.bestchoice.type.CommentSort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/comments")
 public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/{commentId}/like")
+    @PostMapping("/api/posts/{postId}/comments")
+    public ResponseEntity<?> createComment(Authentication authentication, @PathVariable long postId,
+                                           @RequestBody @Valid CreateCommentForm commentForm) {
+        commentService.createComment(authentication, postId, commentForm);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/api/comments/{commentId}/like")
     public ResponseEntity<?> likeComment(Authentication authentication, @PathVariable long commentId) {
         commentService.likeComment(authentication, commentId);
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/{commentId}/unlike")
+    @PostMapping("/api/comments/{commentId}/unlike")
     public ResponseEntity<?> unlikeComment(Authentication authentication, @PathVariable long commentId) {
         commentService.unlikeComment(authentication, commentId);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{commentId}")
+    @DeleteMapping("/api/comments/{commentId}")
     public ResponseEntity<?> deleteComment(Authentication authentication, @PathVariable long commentId) {
         commentService.deleteComment(authentication, commentId);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/posts/{postId}/comments")
+    public ResponseEntity<?> getComments(@PathVariable long postId, @RequestParam(defaultValue = "0") int page,
+                                         @RequestParam(defaultValue = "10") int size,
+                                         @RequestParam(defaultValue = "LATEST") CommentSort sort) {
+        return ResponseEntity.ok(commentService.getComments(postId, page, size, sort));
     }
 }
