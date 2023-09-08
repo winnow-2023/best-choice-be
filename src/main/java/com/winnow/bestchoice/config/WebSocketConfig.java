d00fc6c32a8856bd41a60ee2fe1428095eb13490
winnow-2023 <142189987+winnow-2023@users.noreply.github.com>
2023-09-08 16:28:08 +0900

    Update WebSocketConfig.java

--- src/main/java/com/winnow/bestchoice/config/WebSocketConfig.java
+++ src/main/java/com/winnow/bestchoice/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*").withSockJS(); // TODO: 나중에 cors 에러 발생한다면 이 부분 수정
+        registry.addEndpoint("/ws-stomp").setAllowedOriginPatterns("*"); // TODO: 나중에 cors 에러 발생한다면 이 부분 수정
     }
 
     @Override
