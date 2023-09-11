83871ee8acf4afcaed35ad5c2190113ca5e59de9
winnow-2023 <142189987+winnow-2023@users.noreply.github.com>
2023-09-11 15:25:44 +0900

    Update StompHandler.java

--- src/main/java/com/winnow/bestchoice/config/handler/StompHandler.java
+++ src/main/java/com/winnow/bestchoice/config/handler/StompHandler.java
@@ -103,7 +103,7 @@ public class StompHandler implements ChannelInterceptor {
     }
 
     private static String getTokenByHeader(StompHeaderAccessor accessor) {
-        return accessor.getFirstNativeHeader("token");
+        return (String) accessor.getHeader("token");
     }
 
 }
