2fdf6b9308bc5189bebd91e857060a800a5bd3b6
kimsungho <tigertjdgh@gmail.com>
2023-09-12 14:11:51 +0900

    refactor: 채팅기능 수정

--- src/main/java/com/winnow/bestchoice/repository/ChatRoomRepository.java
+++ src/main/java/com/winnow/bestchoice/repository/ChatRoomRepository.java
@@ -29,23 +29,17 @@ public class ChatRoomRepository {
     private static final String CHAT_ROOMS = "CHAT_ROOM"; // 채팅룸 저장
     public static final String USER_COUNT = "USER_COUNT"; // 채팅룸에 입장한 클라이언트수 저장
 
-    private final RedisMessageListenerContainer redisMessageListener;
-    private final RedisSubscriber redisSubscriber;
+
     private final PostRepository postRepository;
-    private final RedisTemplate<String, Object> redisTemplate;
-    private HashOperations<String, String, ChatRoom> opsHashChatRoom;
-    private Map<String, ChannelTopic> topics;
 
-    @PostConstruct
-    private void init() {
-        opsHashChatRoom = redisTemplate.opsForHash();
-        topics = new HashMap<>();
-    }
-
+    @Resource(name = "redisTemplate")
+    private HashOperations<String, String, ChatRoom> hashOpsChatRoom;
+    @Resource(name = "redisTemplate")
+    private ValueOperations<String, String> valueOps;
 
     // 특정 채팅방 조회
     public ChatRoom findChatRoomByRoomId(String roomId) {
-        return (ChatRoom) Optional.ofNullable(redisTemplate.opsForHash().get(CHAT_ROOMS, roomId)).orElseThrow(
+        return Optional.ofNullable(hashOpsChatRoom.get(CHAT_ROOMS, roomId)).orElseThrow(
                 () -> new CustomException(ErrorCode.CHATROOM_NOT_FOUND)
         );
     }
@@ -53,12 +47,12 @@ public class ChatRoomRepository {
     // 채팅방 목록 조회
     public ChatRoomPage<List<ChatRoomResponse>> findAllChatRoom(int pageNumber, int pageSize) {
         ArrayList<ChatRoomResponse> chatRooms = new ArrayList<>();
-        Set<Object> roomIds = redisTemplate.opsForHash().keys(CHAT_ROOMS);
+        Set<String> roomIds = hashOpsChatRoom.keys(CHAT_ROOMS);
 
-        for (Object roomId : roomIds) {
-            Post post = postRepository.findById(Long.parseLong((String) roomId)).orElseThrow(
+        for (String roomId : roomIds) {
+            Post post = postRepository.findById(Long.parseLong(roomId)).orElseThrow(
                     () -> new CustomException(ErrorCode.POST_NOT_FOUND));
-            ChatRoom chatRoom = (ChatRoom) redisTemplate.opsForHash().get(CHAT_ROOMS, roomId);
+            ChatRoom chatRoom =  hashOpsChatRoom.get(CHAT_ROOMS, roomId);
 
             ChatRoomResponse chatRoomResponse = ChatRoomResponse.fromEntity(post, Objects.requireNonNull(chatRoom));
 
@@ -74,45 +68,31 @@ public class ChatRoomRepository {
     // 채팅방 생성
     public ChatRoom createChatRoom(String roomId) {
         ChatRoom chatRoom = ChatRoom.create(roomId);
-        redisTemplate.opsForHash().put(CHAT_ROOMS, roomId, chatRoom);
+        hashOpsChatRoom.put(CHAT_ROOMS, roomId, chatRoom);
         return chatRoom;
-    }
-
-    // 채팅방 입장
-    public void enterChatRoom(String roomId) {
-        ChannelTopic topic = topics.get(roomId);
-        if (topic == null)
-            topic = new ChannelTopic(roomId);
-        redisMessageListener.addMessageListener((MessageListener) redisSubscriber, topic);
-        topics.put(roomId, topic);
-    }
-
-
-    public ChannelTopic getTopic(String roomId) {
-        return topics.get(roomId);
     }
 
 
     // 채팅방 유저수 조회
     public long getUserCount(String roomId) {
-        return Long.parseLong((String) Optional.ofNullable(redisTemplate.opsForValue().get(USER_COUNT + "_" + roomId)).orElse("0"));
+        return Long.parseLong( Optional.ofNullable(valueOps.get(USER_COUNT + "_" + roomId)).orElse("0"));
     }
 
     // 채팅방에 입장한 유저수 +1
     public long plusUserCount(String roomId) {
-        return Optional.ofNullable(redisTemplate.opsForValue().increment(USER_COUNT + "_" + roomId)).orElse(0L);
+        return Optional.ofNullable(valueOps.increment(USER_COUNT + "_" + roomId)).orElse(0L);
     }
 
     // 채팅방에 입장한 유저수 -1
     public long minusUserCount(String roomId) {
-        return Optional.ofNullable(redisTemplate.opsForValue().decrement(USER_COUNT + "_" + roomId))
+        return Optional.ofNullable(valueOps.decrement(USER_COUNT + "_" + roomId))
                 .filter(count -> count > 0).orElse(0L);
     }
 
     // 채팅방 삭제
     public void deleteChatRoom(String roomId) {
-        redisTemplate.opsForHash().delete(CHAT_ROOMS, roomId);
-        redisTemplate.opsForValue().getAndDelete(USER_COUNT + "_" + roomId);
+        hashOpsChatRoom.delete(CHAT_ROOMS, roomId);
+        valueOps.getAndDelete(USER_COUNT + "_" + roomId);
     }
 
 }
