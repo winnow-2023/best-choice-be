1fa6cdf62c6cab148e8aeb573489f9f4b397e0cb
winnow-2023 <142189987+winnow-2023@users.noreply.github.com>
2023-09-12 21:54:53 +0900

    Update MemberController.java

--- src/main/java/com/winnow/bestchoice/controller/MemberController.java
+++ src/main/java/com/winnow/bestchoice/controller/MemberController.java
@@ -1,18 +1,14 @@
 package com.winnow.bestchoice.controller;
 
-import com.winnow.bestchoice.config.jwt.TokenProvider;
 import com.winnow.bestchoice.model.request.CheckNicknameRequest;
 import com.winnow.bestchoice.model.request.UpdateNicknameRequest;
-import com.winnow.bestchoice.model.response.CheckNicknameResponse;
 import com.winnow.bestchoice.model.response.MemberDetailRes;
 import com.winnow.bestchoice.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @RestController
@@ -32,7 +28,7 @@ public class MemberController {
     public ResponseEntity<?> checkNickname(@RequestBody CheckNicknameRequest request) {
         Boolean result = memberService.validNickname(request.getNickname());
 
-        return ResponseEntity.ok().body(new CheckNicknameResponse(result));
+        return ResponseEntity.ok().body(result);
     }
 
     @GetMapping("/mypage")
